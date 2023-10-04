0a1bccb863500bc579b6a6b7c078a2b96dfda316
Sanjeev1008yadav <130984035+Sanjeev1008yadav@users.noreply.github.com>
2023-10-04 18:19:02 +0530

    addExpenses

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function ExpenseItem() {
+  return (
+    <div>
+        <h1>Expense Items</h1>
+        <p>Food Rs 10</p>
+        <p>Petrol Rs 100</p>
+        <p>Movie Rs 200</p>
+    </div>
+  )
+}
+
+export default ExpenseItem
